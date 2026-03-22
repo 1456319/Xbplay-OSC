b9633645d490d17918d0d5672363fde047374dd2
1456319 <117888838+1456319@users.noreply.github.com>
2026-03-22 04:39:59 +0000

    Add stubbed replacements for missing proprietary backend classes
    Added dummy files for network.BindService, Interfaces.SmartglassEvents,
    and constants.SystemInputButtonMappings to allow the open-source project
    to compile. These files were originally missing from the repository.

---
+++ app/src/main/java/Interfaces/SmartglassEvents.java
@@ -0,0 +1,10 @@
+package Interfaces;
+
+public interface SmartglassEvents {
+    void xboxDiscovered();
+    void xboxConnected();
+    void xboxDisconnected();
+    // void genericMessage(String type, String msg);
+    // void vibrate();
+    // void setOrientationValue(String value);
+}
